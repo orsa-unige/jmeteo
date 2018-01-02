12358aee4f92e098423d819be47dcaafc3441e15
vertighel <davide.ricci82@gmail.com>
2018-01-02 23:56:33 +0100

    d3 list with the new and the old json format

--- client.js
+++ client.js
@@ -1,11 +1,48 @@
 
-d3.json("./stanotte.json", function(err,json) {
+    
+d3.json("./object-list.json", function(err,json) {
 
     var desc= d3.select('body')
 	.append("ul");
 
+    var fields=["datetime", "rtInsideTemp", "rtBaroCurr"];
+    
     desc
 	.selectAll('li')
+        .data(fields)
+        .enter()
+        .append('li')
+        .text(function(d){ return d  +" → "+json[0][d].description; } );
+    
+    var ul = d3.select('body')
+	.append('ul');
+
+    ul
+        .append('li')
+        .text(fields.join(" → "));
+    
+    var li=ul.selectAll('li')
+        .data(json)
+        .enter()
+        .append('li')
+        .text(function(d){
+	    var tri=[];
+	    fields.forEach(
+		function(f){
+		    tri.push(d[f].value);
+		});
+	    return tri.join(" → ");
+	});
+ 
+});
+
+d3.json("./array-list.json", function(err,json) {
+    
+    var desc= d3.select('body')
+    	.append("ul");
+
+    desc
+    	.selectAll('li')
         .data( [json[0].data[39], json[0].data[0], json[0].data[2] ]  )
         .enter()
         .append('li')
@@ -22,5 +59,5 @@ d3.json("./stanotte.json", function(err,json) {
         .enter()
         .append('li')
         .text(function(d,i){ return d.data[39].value+" →  "+d.data[0].value+" →  "+d.data[2].value; } );
-    
-});
+
+    });
